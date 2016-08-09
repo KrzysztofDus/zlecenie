a7387342f67d89342a65e98494e3a6c66b4d9720
Mateusz Woźniak <mateusz.wozniak@sap.com>
2016-08-09 14:29:07 +0200

    Gruntfile suggestions

--- gruntfile.js
+++ gruntfile.js
@@ -1,44 +1,45 @@
 module.exports = function (grunt) {
 // Project configuration.
- grunt.initConfig({ 
+//Gruntfile.js should start with Capital letter
+ grunt.initConfig({
 
 
- 	sass: { 
- 		options: { 
+ 	sass: {
+ 		options: {
  			sourceMap: true
- 		  }, 
- 		  dist: { 
- 			 files: { 
- 			 	'css/main.css': 'sass/main.sass' 
- 			 } 
- 		  } 
- 		}, 
+ 		  },
+ 		  dist: {
+ 			 files: {
+ 			 	'css/main.css': 'sass/main.sass'
+ 			 }
+ 		  }
+ 		},
 
- 
 
- 	imagemin: { 
- 		dynamic: { 
- 			files: [{ 
- 				expand: true, 
- 				cwd: 'images/', 
- 				src: ['**/*.{png,jpg,gif}'], 
- 				dest: 'images/build/' 
- 			}] 
- 		} 
+
+ 	imagemin: {
+ 		dynamic: {
+ 			files: [{
+ 				expand: true,
+ 				cwd: 'images/',
+ 				src: ['**/*.{png,jpg,gif}'],
+ 				dest: 'images/build/'
+ 			}]
+ 		}
  	},
 
-
- 	watch: { 
- 		scripts: { 
- 			files: ['sass/*.sass'], 
- 			tasks: ['sass'], 
- 			options: { 
- 				spawn: false, 
- 			}, 
- 		} 
+//Add watch task for images so they woulb be minified when new will be added
+ 	watch: {
+ 		scripts: {
+ 			files: ['sass/*.sass'],
+ 			tasks: ['sass'],
+ 			options: {
+ 				spawn: false,
+ 			},
+ 		}
  	}
  });
-  // Load the plugins tasks 
+  // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
